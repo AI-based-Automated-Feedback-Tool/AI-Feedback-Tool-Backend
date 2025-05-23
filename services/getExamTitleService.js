eb008490a5d849f2848ce32cbf71610a03db03a9
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-05-23 20:08:17 +0300

    Add function to fetch exam titles by course_id from Supabase

---
+++ services/getExamTitleService.js
@@ -0,0 +1,18 @@
+const {supabase} = require('../supabaseClient')
+
+async function getExamTitle({course_id}) {
+    const { data,error } = await supabase
+        .from('exams')
+        .select('exam_id, title')
+        .eq('course_id', course_id)
+
+    if (error) {
+        const err = new Error("Failed to get the exam title");
+        err.status = 500;
+        throw err;
+    }
+
+    return data;
+}
+
+module.exports = getExamTitle;
