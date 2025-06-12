451259858b5f423f608e2ff1ab9e1e2d6b91cef5
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-12 22:39:54 +0300

    implement file upload logic using Supabase in fileUploadService

---
+++ services/createEssayQuestion/fileUploadService.js
@@ -0,0 +1,23 @@
+const { supabase } = require('../../supabaseClient');
+
+async function saveFileToSupabase(file) {
+    const filePath = `attachments/${Date.now()}_${file.originalname}`;
+    const {data , error} = await supabase.storage
+        .from('attachments')
+        .upload(filePath, file.buffer, {
+            contentType: file.mimetype,
+            upsert: true
+        });
+        if (error) {
+            const err = new Error("Failed to upload file");
+            console.log(error);
+            err.status = 500;
+            throw err;
+        }
+
+        const {publicUrl} = supabase.storage
+            .from('attachments')
+            .getPublicUrl(filePath).data;
+        return publicUrl;
+}
+module.exports = {saveFileToSupabase};
