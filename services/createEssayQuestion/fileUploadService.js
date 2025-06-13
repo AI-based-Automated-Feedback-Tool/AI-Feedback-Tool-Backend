1bb67e97acac82ddbb5e02f57c07a4db69f11518
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-13 15:52:39 +0300

    Check errors

--- services/createEssayQuestion/fileUploadService.js
+++ services/createEssayQuestion/fileUploadService.js
@@ -2,22 +2,27 @@ const { supabase } = require('../../supabaseClient');
 
 async function saveFileToSupabase(file) {
     const filePath = `attachments/${Date.now()}_${file.originalname}`;
-    const {data , error} = await supabase.storage
+    const {data:upload , error:uploadError} = await supabase.storage
         .from('essay-attachments')
         .upload(filePath, file.buffer, {
             contentType: file.mimetype,
-            upsert: true
+            upsert: false
         });
-        if (error) {
+        if (uploadError) {
             const err = new Error("Failed to upload file");
-            console.log(error);
+            console.log(uploadError);
             err.status = 500;
             throw err;
         }
 
-        const {publicUrl} = supabase.storage
+        const {data:publicUrlData} = supabase.storage
             .from('essay-attachments')
-            .getPublicUrl(filePath).data;
-        return publicUrl;
+            .getPublicUrl(filePath);
+        if (!publicUrlData) {
+            const err = new Error("Failed to get public URL for the file");
+            err.status = 500;
+            throw err;
+        }
+        return publicUrlData;
 }
 module.exports = {saveFileToSupabase};
