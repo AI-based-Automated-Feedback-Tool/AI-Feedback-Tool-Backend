f71e7e59649fc2b07ae744fc002d1ce2d963c181
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-13 14:38:08 +0300

    Seccessfully store an attachment

--- services/createEssayQuestion/fileUploadService.js
+++ services/createEssayQuestion/fileUploadService.js
@@ -3,7 +3,7 @@ const { supabase } = require('../../supabaseClient');
 async function saveFileToSupabase(file) {
     const filePath = `attachments/${Date.now()}_${file.originalname}`;
     const {data , error} = await supabase.storage
-        .from('attachments')
+        .from('essay-attachments')
         .upload(filePath, file.buffer, {
             contentType: file.mimetype,
             upsert: true
@@ -16,7 +16,7 @@ async function saveFileToSupabase(file) {
         }
 
         const {publicUrl} = supabase.storage
-            .from('attachments')
+            .from('essay-attachments')
             .getPublicUrl(filePath).data;
         return publicUrl;
 }
