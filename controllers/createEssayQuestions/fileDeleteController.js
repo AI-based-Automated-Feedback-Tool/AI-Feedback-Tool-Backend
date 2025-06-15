f76b4889678f332a80f91ac26a9250e739a890ff
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-16 01:04:36 +0300

    implement controller to handle attachment deletion requests

---
+++ controllers/createEssayQuestions/fileDeleteController.js
@@ -0,0 +1,16 @@
+const {deleteFileFromSupabase} = require("../../services/createEssayQuestion/fileDeleteService");
+
+const deleteEssayAttachment  = async (req,res) => {
+    try{
+        const filePath = req.body.filePath; 
+        if (!filePath) {
+            return res.status(400).json({ message: "No file path provided" });
+        }
+        const result = await deleteFileFromSupabase(filePath);
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(error.status || 500).json({ error: error.message || "Internal Server Error" });
+    }
+}
+
+module.exports = { deleteEssayAttachment };
