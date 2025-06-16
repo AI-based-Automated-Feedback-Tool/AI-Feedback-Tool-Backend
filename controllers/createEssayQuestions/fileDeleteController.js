618859514a13c903f8dfc009b59504c0e619cdfa
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-16 12:20:55 +0300

    Fix error

--- controllers/createEssayQuestions/fileDeleteController.js
+++ controllers/createEssayQuestions/fileDeleteController.js
@@ -3,10 +3,11 @@ const {deleteFileFromSupabase} = require("../../services/createEssayQuestion/fil
 const deleteEssayAttachment  = async (req,res) => {
     try{
         const filePath = req.body.filePath; 
+        const decodedFilePath = decodeURIComponent(filePath);
         if (!filePath) {
             return res.status(400).json({ message: "No file path provided" });
         }
-        const result = await deleteFileFromSupabase(filePath);
+        const result = await deleteFileFromSupabase(decodedFilePath);
         return res.status(200).json(result);
     } catch (error) {
         return res.status(error.status || 500).json({ error: error.message || "Internal Server Error" });
--- services/createEssayQuestion/fileDeleteService.js
+++ services/createEssayQuestion/fileDeleteService.js
@@ -1,12 +1,17 @@
 const { supabase } = require('../../supabaseClient');
 
 async function deleteFileFromSupabase(filePath) {
-    const {error:deleteError} = await supabase
+    if (!filePath) {
+        const err = new Error("File path is required");
+        err.status = 400;
+        throw err;
+    }
+    const {data, error:deleteError} = await supabase
         .storage
         .from('essay-attachments')
         .remove([filePath]);
 
-        if (deleteError) {
+        if (deleteError || !data || data.length === 0) {
             const err = new Error("Failed to delete file");
             err.status = 500;
             throw err;
