e4fe79d1f49fa2363765fa45c68a1dd8aab2acc1
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-06-21 01:43:24 +0300

    implement service to fetch submitted essay answers by submission ID

---
+++ services/reportsTeacher/getSubmittedEssayAnswers.js
@@ -0,0 +1,19 @@
+const {supabase} = require('../../supabaseClient')
+
+async function getSubmittedEssayAnswers({submission_id}) {
+    const { data,error } = await supabase
+        .from('essay_exams_submissions_answers')
+        .select('*')
+        .eq('submission_id', submission_id)
+
+    if (error) {
+        console.error("Supabase error:", error);
+        const err = new Error("Failed to get the submitted answer details");
+        err.status = 500;
+        throw err;
+    }
+
+    return data;
+}
+
+module.exports = getSubmittedEssayAnswers;
