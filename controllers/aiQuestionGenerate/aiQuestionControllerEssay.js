de3bc52d9e28488d002052bd1d2ca63b7e2d2d41
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-10-27 15:32:06 +0200

    Generate essay questions using deepseek

--- controllers/aiQuestionGenerate/aiQuestionControllerEssay.js
+++ controllers/aiQuestionGenerate/aiQuestionControllerEssay.js
@@ -1,4 +1,5 @@
 const {generateEssayQuestions } = require('../../services/aiQuestionGenerate/aiQuestionServiceEssay');
+const { generateEssayQuestionsDeepseek } = require('../../services/aiQuestionGenerate/aiQuestionServiceEssayDeepSeek');
 
 const generateAIQuestionsEssay = async (req, res) => {
     try {
@@ -11,9 +12,16 @@ const generateAIQuestionsEssay = async (req, res) => {
             wordLimitAI,
             pointsAI,
             noOfQuestion,
-            gradingNotesAI
-            } = req.body;
-        const questions = await generateEssayQuestions(topic, difficultyLevel, guidance, keyConcepts, doNotInclude, wordLimitAI, pointsAI, noOfQuestion, gradingNotesAI);
+            gradingNotesAI,
+            aiModel
+        } = req.body;
+
+        let questions;
+        if (aiModel === "deepseek") {
+            questions = await generateEssayQuestionsDeepseek(topic, difficultyLevel, guidance, keyConcepts, doNotInclude, wordLimitAI, pointsAI, noOfQuestion, gradingNotesAI);
+        } else {
+            questions = await generateEssayQuestions(topic, difficultyLevel, guidance, keyConcepts, doNotInclude, wordLimitAI, pointsAI, noOfQuestion, gradingNotesAI);
+        }
         res.status(200).json({questions});
     } catch (error) {
         console.error('Error generating questions:', error);
