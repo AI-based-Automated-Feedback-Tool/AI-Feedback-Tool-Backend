77a2df9cda4141070566eae922c3cffaee0144f6
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2025-10-29 13:22:43 +0200

    Add middleware to route

--- routes/aiQuestionGenerate/aiQuestionRoutes.js
+++ routes/aiQuestionGenerate/aiQuestionRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {generateAIQuestions} = require('../../controllers/aiQuestionGenerate/aiQuestionController.js');
+const { supabaseAuth } = require('../../middleware/supabaseAuth.js');
 
-router.post('/', generateAIQuestions);
+router.post('/', supabaseAuth, generateAIQuestions);
 
 module.exports = router;
